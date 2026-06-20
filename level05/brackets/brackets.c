c7e35104b90144ec350472a45b6b01df57ea8248
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-06-19 21:27:52 -0400

    Update brackets.c

--- level05/brackets/brackets.c
+++ level05/brackets/brackets.c
@@ -1,42 +1,57 @@
 # include <unistd.h>
-# include <stdio.h>
-
 
 int is_open(char c)
 {
-    return (c == '{' || c == '(' || c == '[');
+    if(c == '{' || c == '[' || c == '(')
+        return 1;
+    return 0;
 }
 int is_close(char c)
 {
-    return (c == '}' || c == ')' || c == ']');
+    if(c == '}' || c == ')' || c == ']')
+        return 1;
+    return 0;
 }
-int close_tag(char c1, char c2)
+int correct_colse(char c1, char c2)
 {
-    if(c1 == '{' && c2 == '}')
-        return 1;
-    else if(c1 == '(' && c2 == ')')
-        return 1;
-    else if(c1 == '[' && c2 == ']')
-        return 1;
+    if(c1 == '{' && c2 == '}') return 1;
+    else if(c1 == '(' && c2 == ')') return 1;
+    else if(c1 == '[' && c2 == ']') return 1;
     else
         return 0;
 }
-int brackets(char *str)
+int is_spaces(char *s)
 {
-    char stack[1024];
+    if(s[0] != 32 && s[0] != 9 && s[0] != '\0')
+        return 0;
+    while(*s && (*s == 32 || *s == 9))
+        s++;
+    if(!*s)
+        return 1;
+    return 0;
+
+}
+int brackets(char *s, int *inter)
+{
     int j = -1;
-    while(*str)
+    char arr[1024];
+
+    while(*s)
     {
-        if(is_open(*str))
-            stack[++j] = *str;
-        else if(j >= 0 && is_close(*str))
+        
+        if(is_open(*s))
         {
-            if(close_tag(stack[j], *str))
+            arr[++j] = *s;
+        }
+        else if(j >= 0 && is_close(*s))
+        {
+            *inter = 1;
+            if(correct_colse(arr[j], *s))
                 j--;
             else
                 return 0;
         }
-        str++;
+        s++;
     }
     return (j == -1);
 }
@@ -45,15 +60,14 @@ int main(int ac, char **av)
     if(ac >= 2)
     {
         int i = 1;
+        int inter;
         while(av[i])
         {
-            if(!brackets(av[i]))
-            {
-                write(1, "error", 5);
-            }
+            inter = 0;
+            if(is_spaces(av[i]) || (brackets(av[i], &inter) && inter))
+                write(1, "OK\n", 3);
             else
-                write(1, "OK", 2);
-            write(1, "\n", 1);
+                write(1, "Error\n", 6);
             i++;
         }
     }
